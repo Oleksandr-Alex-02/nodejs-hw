157c98a2fca2ead2adfd1002b0841a9603b743b6
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-10-20 00:09:39 +0300

    validations

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,9 @@
+import { Joi } from 'celebrate';
+
+const bodySchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  age: Joi.number().integer().min(12).max(65).required(),
+  gender: Joi.string().valid('male', 'female', 'other').required(),
+  avgMark: Joi.number().min(2).max(12).required(),
+  onDuty: Joi.boolean(),
+});
