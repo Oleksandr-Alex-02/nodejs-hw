2a6f803f2acf9727910387e507c7697907571c92
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-11-25 21:19:04 +0200

    resetPassword

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -115,6 +115,8 @@ export const requestResetEmail = async (req, res, next) => {
     { expiresIn: '15m' },
   );
 
+  console.log(resetToken)
+
   const templatePath = path.resolve('src/templates/reset-password-email.html');
   const templateSource = await fs.readFile(templatePath, 'utf-8');
   const template = handlebars.compile(templateSource);
